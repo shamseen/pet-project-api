3cb5b67e23f82ca8f669bdd2faa7801cf25709a6
shamseen <shamseen.c@gmail.com>
2021-09-21 17:05:08 -0400

    basic server setup sans mongoose

---
+++ server.js
@@ -0,0 +1,47 @@
+/* --- Required modules --- */
+require("dotenv").config(); // inject .env into process.env
+const express = require("express"); // http server
+const cors = require("cors"); // expose resources for external websites
+const mongoose = require("mongoose"); // talks to mongo db
+
+/* --- App variables --- */
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+/* --- Connect to database --- */
+// mongoose.connect(process.env.MONGO_URI, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   connectTimeoutMS: 3000, // Extending in case heroku is slow
+// });
+// // confirm connection
+// mongoose.connection.once("connected", () =>
+//   console.log(">> mongoose is connected to mongoDB")
+// );
+
+/* --- Middleware for CRUD & controller routing --- */
+app.use(express.json()); // reads incoming PUT/POST as json
+
+app.use((req, res, next) => {
+  console.log(req.body); // logging the request
+  next(); // run next middleware func
+});
+
+app.use(cors()); // exposes endpoints for apps to request
+
+// /* --- Routes --- */
+// app.use("/user", require("./controllers/usersController"));
+// app.use("/search", require("./controllers/searchController"));
+
+// app.get("/", (req, res) => {
+//   res.send(`<h1>Pet Project API</h1>`);
+// });
+
+/* --- Leggggoooooooo --- */
+app.listen(PORT, () =>
+  console.log(
+    `>> API Server: Listening on port ${PORT}. waiting for database...`
+  )
+);
