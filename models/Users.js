79beeb10b33bf3cd8410e100a961e410d7cb7930
Ashley Sands <ashley.sands01@gmail.com>
2021-09-22 12:27:35 -0500

    updated users and preferences models

--- models/Users.js
+++ models/Users.js
@@ -6,16 +6,7 @@ const UserSchema = new Schema(
     name: { type: String, required: true, unique: true },
     email: { type: String, required: true , trim: true, unique: true },
     passwordDigest: { type: String, required: true, select: false },
-    behavior: { type: String, required: false },
-    disabled: { type: Boolean, required: false },
-    daysInShelter: { type: Number, required: false },
-    furType: { type: String, required: false },
-    size: { type: String, required: false },
-    medications: {type: String, required: false},
-    sex: { type: String, required: false },
-    activityLevel: { type: String, required: false },
-    breed: { type: String, required: false },
-    age: {type: String, required: false}
+    
   },
   {timestamps: true}
 )
